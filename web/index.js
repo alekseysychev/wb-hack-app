0bf8f2d2e472176c25c0e61efd1223c1b5adc315
Сычёв Алексей Владимирович <79252045491@yandex.ru>
2021-11-14 13:50:57 +0300

    add ajax

--- web/index.js
+++ web/index.js
@@ -3,19 +3,35 @@ const list =  document.getElementById('list')
 
 function sendRequest(input) {
     console.log(input)
+
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function() {
+        if (xmlhttp.readyState == XMLHttpRequest.DONE) {   // XMLHttpRequest.DONE == 4
+           if (xmlhttp.status == 200) {
+               let result =  JSON.parse(xmlhttp.response);
+               for (let i = 0; i < result.length; i++) {
+                const liRod = document.createElement('li')
+                liRod.innerHTML = result[i]
+                liRod.onclick = () => {
+                    SearchInput.value = liRod.innerHTML
+                }
+                list.appendChild(liRod)
+            }
+           }
+           else if (xmlhttp.status == 400) {
+              alert('There was an error 400');
+           }
+           else {
+               alert('something else other than 200 was returned');
+           }
+        }
+    };
+    xmlhttp.open("GET", "/api", true);
+     xmlhttp.send();
     return ['1', '2', '3']
 }
 
 SearchInput.onchange = () => {
     console.log('here')
-    result = sendRequest(SearchInput.value)
-    for (let i = 0; i < result.length; i++) {
-        resultElement = result[i]
-        const liRod = document.createElement('li')
-        liRod.innerHTML = resultElement
-        liRod.onclick = () => {
-            SearchInput.value = liRod.innerHTML
-        }
-        list.appendChild(liRod)
-    }
+    sendRequest(SearchInput.value);
 }
