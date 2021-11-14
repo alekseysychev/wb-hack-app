ed1174d2dc0d56056b1d4a7a56858d952b0ff96f
Сычёв Алексей Владимирович <79252045491@yandex.ru>
2021-11-14 14:12:38 +0300

    форматирование & добавление очистки старого поиска

--- web/index.js
+++ web/index.js
@@ -1,37 +1,37 @@
 const SearchInput = document.getElementById('search')
-const list =  document.getElementById('list')
+const list = document.getElementById('list')
 
 function sendRequest(input) {
-    console.log(input)
-
     var xmlhttp = new XMLHttpRequest();
-    xmlhttp.onreadystatechange = function() {
+    xmlhttp.onreadystatechange = function () {
         if (xmlhttp.readyState == XMLHttpRequest.DONE) {   // XMLHttpRequest.DONE == 4
-           if (xmlhttp.status == 200) {
-               let result =  JSON.parse(xmlhttp.response);
-               for (let i = 0; i < result.length; i++) {
-                const liRod = document.createElement('li')
-                liRod.innerHTML = result[i]
-                liRod.onclick = () => {
-                    SearchInput.value = liRod.innerHTML
+            console.log('asd');
+            if (xmlhttp.status == 200) {
+                let result = JSON.parse(xmlhttp.response);
+                list.innerHTML = '';
+                for (let i = 0; i < result.length; i++) {
+                    const liRod = document.createElement('li')
+                    liRod.innerHTML = result[i]
+                    liRod.onclick = () => {
+                        SearchInput.value = liRod.innerHTML;
+                        sendRequest(SearchInput.value);
+                    }
+                    list.appendChild(liRod)
                 }
-                list.appendChild(liRod)
             }
-           }
-           else if (xmlhttp.status == 400) {
-              alert('There was an error 400');
-           }
-           else {
-               alert('something else other than 200 was returned');
-           }
+            else if (xmlhttp.status == 400) {
+                alert('There was an error 400');
+            }
+            else {
+                alert('something else other than 200 was returned');
+            }
         }
     };
     xmlhttp.open("GET", "/api", true);
-     xmlhttp.send();
+    xmlhttp.send();
     return ['1', '2', '3']
 }
 
-SearchInput.onchange = () => {
-    console.log('here')
+SearchInput.onkeyup = () => {
     sendRequest(SearchInput.value);
 }
