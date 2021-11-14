3cb9966720ee6eb63b78f39b9232cb2912fbc382
Сычёв Алексей Владимирович <79252045491@yandex.ru>
2021-11-14 14:34:06 +0300

    fix

--- web/index.js
+++ web/index.js
@@ -19,10 +19,10 @@ function sendRequest(input) {
                 }
             }
             else if (xmlhttp.status == 400) {
-                alert('There was an error 400');
+                // alert('There was an error 400');
             }
             else {
-                alert('something else other than 200 was returned');
+                // alert('something else other than 200 was returned');
             }
         }
     };
